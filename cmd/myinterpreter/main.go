986aa2f34bed9ccbab6c5699d393c2babe629134
Haider Alshamma <haidera@nulogy.com>
2024-08-21 13:35:24 -0400

    Stage 4 refactored

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -1,53 +1,83 @@
 package main
 
 import (
+    "bufio"
     "fmt"
+    "io"
     "os"
-    "strings"
 )
 
+type TokenType string
+
+const (
+    LeftParen  TokenType = "LEFT_PAREN"
+    RightParen TokenType = "RIGHT_PAREN"
+    LeftBrace  TokenType = "LEFT_BRACE"
+    RightBrace TokenType = "RIGHT_BRACE"
+    EOF        TokenType = "EOF"
+)
+
+type Token struct {
+    Type   TokenType
+    Lexeme string
+}
+
 func main() {
-    fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
-
-    if len(os.Args) < 3 {
-        fmt.Fprintln(os.Stderr, "Usage: ./your_program.sh tokenize <filename>")
+    if err := run(); err != nil {
+        fmt.Fprintf(os.Stderr, "Error: %v\n", err)
         os.Exit(1)
     }
+}
 
-    command := os.Args[1]
-
-    if command != "tokenize" {
-        fmt.Fprintf(os.Stderr, "Unknown command: %s\n", command)
-        os.Exit(1)
+func run() error {
+    if len(os.Args) != 3 || os.Args[1] != "tokenize" {
+        return fmt.Errorf("usage: %s tokenize <filename>", os.Args[0])
     }
 
-    filename := os.Args[2]
-    fileContents, err := os.ReadFile(filename)
+    file, err := os.Open(os.Args[2])
     if err != nil {
-        fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
-        os.Exit(1)
+        return fmt.Errorf("error opening file: %w", err)
+    }
+    defer file.Close()
+
+    tokens, err := tokenize(file)
+    if err != nil {
+        return fmt.Errorf("error tokenizing: %w", err)
     }
 
-    if len(fileContents) > 0 {
-        var slice []string
+    printTokens(tokens)
+    return nil
+}
 
-        for _, v := range fileContents {
-            switch string(v) {
-            case "(":
-                slice = append(slice, "LEFT_PAREN ( null")
-            case ")":
-                slice = append(slice, "RIGHT_PAREN ) null")
-            case "{":
-                slice = append(slice, "LEFT_BRACE { null")
-            case "}":
-                slice = append(slice, "RIGHT_BRACE } null")
-            }
+func tokenize(r io.Reader) ([]Token, error) {
+    scanner := bufio.NewScanner(r)
+    scanner.Split(bufio.ScanRunes)
+
+    var tokens []Token
+    for scanner.Scan() {
+        char := scanner.Text()
+        switch char {
+        case "(":
+            tokens = append(tokens, Token{LeftParen, char})
+        case ")":
+            tokens = append(tokens, Token{RightParen, char})
+        case "{":
+            tokens = append(tokens, Token{LeftBrace, char})
+        case "}":
+            tokens = append(tokens, Token{RightBrace, char})
         }
+    }
 
-        slice = append(slice, "EOF  null")
+    if err := scanner.Err(); err != nil {
+        return nil, fmt.Errorf("error scanning input: %w", err)
+    }
 
-        fmt.Println(strings.Join(slice, "\n"))
-    } else {
-        fmt.Println("EOF  null")
+    tokens = append(tokens, Token{EOF, ""})
+    return tokens, nil
+}
+
+func printTokens(tokens []Token) {
+    for _, token := range tokens {
+        fmt.Printf("%s %s null\n", token.Type, token.Lexeme)
     }
 }

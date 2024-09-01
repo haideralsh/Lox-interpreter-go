f72fd2249e65c6d3dc5b0cdb1627e9e3ef6321d0
Haider Alshamma <haidera@nulogy.com>
2024-09-01 14:26:24 -0400

    Handle unsupported characters

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -1,35 +1,53 @@
 package main
 
 import (
-	"fmt"
-	"os"
+    "fmt"
+    "os"
 
-	"github.com/codecrafters-io/interpreter-starter-go/token"
+    "github.com/codecrafters-io/interpreter-starter-go/token"
 )
 
 func main() {
-	if err := run(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		os.Exit(1)
-	}
+    if err := run(); err != nil {
+        fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+        os.Exit(1)
+    }
 }
 
 func run() error {
-	if len(os.Args) != 3 || os.Args[1] != "tokenize" {
-		return fmt.Errorf("usage: %s tokenize <filename>", os.Args[0])
-	}
+    if len(os.Args) != 3 || os.Args[1] != "tokenize" {
+        return fmt.Errorf("usage: %s tokenize <filename>", os.Args[0])
+    }
 
-	file, err := os.Open(os.Args[2])
-	if err != nil {
-		return fmt.Errorf("error opening file: %w", err)
-	}
-	defer file.Close()
+    file, err := os.Open(os.Args[2])
+    if err != nil {
+        return fmt.Errorf("error opening file: %w", err)
+    }
+    defer file.Close()
 
-	tokens, err := token.Tokenize(file)
-	if err != nil {
-		return fmt.Errorf("error tokenizing: %w", err)
-	}
+    tokens, err := token.Tokenize(file)
+    if err != nil {
+        return fmt.Errorf("error tokenizing: %w", err)
+    }
 
-	token.Print(tokens)
-	return nil
+    containsError := false
+    for _, t := range tokens {
+        if !containsError {
+            containsError = t.Type == token.Error
+        }
+
+        if t.Type == token.Error {
+            fmt.Fprint(os.Stderr, t.String())
+        } else {
+            fmt.Fprint(os.Stdout, t.String())
+        }
+    }
+
+    if containsError {
+        os.Exit(65)
+    } else {
+        os.Exit(0)
+    }
+
+    return nil
 }
--- token/token.go
+++ token/token.go
@@ -1,73 +1,85 @@
 package token
 
 import (
-	"bufio"
-	"fmt"
-	"io"
+    "bufio"
+    "fmt"
+    "io"
 )
 
 type Type string
 
 const (
-	LeftParen  Type = "LEFT_PAREN"
-	RightParen Type = "RIGHT_PAREN"
-	LeftBrace  Type = "LEFT_BRACE"
-	RightBrace Type = "RIGHT_BRACE"
-	EOF        Type = "EOF"
-	Comma      Type = "COMMA"
-	Dot        Type = "DOT"
-	Minus      Type = "MINUS"
-	Plus       Type = "PLUS"
-	SemiColon  Type = "SEMICOLON"
-	Star       Type = "STAR"
+    LeftParen  Type = "LEFT_PAREN"
+    RightParen Type = "RIGHT_PAREN"
+    LeftBrace  Type = "LEFT_BRACE"
+    RightBrace Type = "RIGHT_BRACE"
+    EOF        Type = "EOF"
+    Comma      Type = "COMMA"
+    Dot        Type = "DOT"
+    Minus      Type = "MINUS"
+    Plus       Type = "PLUS"
+    SemiColon  Type = "SEMICOLON"
+    Star       Type = "STAR"
+    Error      Type = "Error"
 )
 
 type Token struct {
-	Type   Type
-	Lexeme string
+    Type   Type
+    Lexeme string
+    line   int
 }
 
 func Tokenize(r io.Reader) ([]Token, error) {
-	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanRunes)
+    scanner := bufio.NewScanner(r)
+    scanner.Split(bufio.ScanRunes)
 
-	var tokens []Token
-	for scanner.Scan() {
-		char := scanner.Text()
-		switch char {
-		case "(":
-			tokens = append(tokens, Token{LeftParen, char})
-		case ")":
-			tokens = append(tokens, Token{RightParen, char})
-		case "{":
-			tokens = append(tokens, Token{LeftBrace, char})
-		case "}":
-			tokens = append(tokens, Token{RightBrace, char})
-		case ",":
-			tokens = append(tokens, Token{Comma, char})
-		case ".":
-			tokens = append(tokens, Token{Dot, char})
-		case "-":
-			tokens = append(tokens, Token{Minus, char})
-		case "+":
-			tokens = append(tokens, Token{Plus, char})
-		case ";":
-			tokens = append(tokens, Token{SemiColon, char})
-		case "*":
-			tokens = append(tokens, Token{Star, char})
-		}
-	}
+    var tokens []Token
+    line := 1
 
-	if err := scanner.Err(); err != nil {
-		return nil, fmt.Errorf("error scanning input: %w", err)
-	}
+    for scanner.Scan() {
+        char := scanner.Text()
 
-	tokens = append(tokens, Token{EOF, ""})
-	return tokens, nil
+        switch char {
+        case "(":
+            tokens = append(tokens, Token{LeftParen, char, line})
+        case ")":
+            tokens = append(tokens, Token{RightParen, char, line})
+        case "{":
+            tokens = append(tokens, Token{LeftBrace, char, line})
+        case "}":
+            tokens = append(tokens, Token{RightBrace, char, line})
+        case ",":
+            tokens = append(tokens, Token{Comma, char, line})
+        case ".":
+            tokens = append(tokens, Token{Dot, char, line})
+        case "-":
+            tokens = append(tokens, Token{Minus, char, line})
+        case "+":
+            tokens = append(tokens, Token{Plus, char, line})
+        case ";":
+            tokens = append(tokens, Token{SemiColon, char, line})
+        case "*":
+            tokens = append(tokens, Token{Star, char, line})
+        case "/n":
+            line++
+        default:
+            tokens = append(tokens, Token{Error, char, line})
+        }
+    }
+
+    if err := scanner.Err(); err != nil {
+        return nil, fmt.Errorf("error scanning input: %w", err)
+    }
+
+    tokens = append(tokens, Token{EOF, "", line})
+    return tokens, nil
 }
 
-func Print(tokens []Token) {
-	for _, token := range tokens {
-		fmt.Printf("%s %s null\n", token.Type, token.Lexeme)
-	}
+func (t Token) String() string {
+    switch t.Type {
+    case Error:
+        return fmt.Sprintf("[line %v] Error: Unexpected character: %v\n", t.line, t.Lexeme)
+    default:
+        return fmt.Sprintf("%s %s null\n", t.Type, t.Lexeme)
+    }
 }
